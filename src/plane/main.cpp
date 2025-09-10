d3c5dd96290459d40a3012d4c5a8cc1f097b803c
bigsas777 <luca.zorzi77@gmail.com>
2025-09-10 18:25:46 +0200

    Created project

---
+++ src/plane/main.cpp
@@ -0,0 +1,10 @@
+
+// #define ESC_PIN 0 // TO SET Actually for the plane code
+
+// Servo ESC; // Actually for the plane code
+
+// ESC.attach(ESC_PIN); // Actually for the plane code
+
+// In plane's code use ESC.writeMicroseconds() for motor control
+
+ // For maximum reach, in the plane set to MIN and enable LNA (default) radio.setPALevel(RF24_PA_MAX);
